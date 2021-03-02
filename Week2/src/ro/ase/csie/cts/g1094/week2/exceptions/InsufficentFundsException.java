2a11ad9f5583af688a72cf9982e967fdada456f6
Mr.Paun-PC <paunalexandru18@stud.ase.ro>
2021-03-02 14:35:14 +0200

    Week 2 changes

--- Week2/src/ro/ase/csie/cts/g1094/week2/exceptions/InsufficentFundsException.java
+++ Week2/src/ro/ase/csie/cts/g1094/week2/exceptions/InsufficentFundsException.java
@@ -1,5 +1,12 @@
 package ro.ase.csie.cts.g1094.week2.exceptions;
 
 public class InsufficentFundsException extends Exception {
-
+	
+	public InsufficentFundsException(String message) {
+		super(message);
+	}
+	
+	public InsufficentFundsException() {
+		
+	}
 }
