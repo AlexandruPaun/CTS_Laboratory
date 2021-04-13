fd8976f3d1735540beaff431a7836d1ac45707e2
AlexandruPaun <paunalexandru18@stud.ase.ro>
2021-04-13 14:35:15 +0300

    Facade Implementation. Adapter started.

---
+++ Week8 - Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/facade/UserProfile.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1094.dp.facade;
+
+public class UserProfile {
+	public void getUserProfile() {
+		System.out.println("Getting user data");
+	}
+	public UserProfile downloadUserProfile() {
+		return new UserProfile();
+	}
+}
